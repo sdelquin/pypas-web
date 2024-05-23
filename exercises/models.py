95b313eb5721bb4ee11f9120c1e142b05d9d2279
Sergio Delgado Quintero <sdelquin@gmail.com>
2024-05-23 20:58:53 +0100

    Add zip management for exercises

--- exercises/models.py
+++ exercises/models.py
@@ -1,3 +1,9 @@
+import glob
+import io
+import zipfile
+from pathlib import Path
+from typing import Iterator
+
 from django.conf import settings
 from django.db import models
 
@@ -7,8 +13,31 @@ class Exercise(models.Model):
     available = models.BooleanField(default=True)
 
     @property
-    def path(self):
+    def path(self) -> Path:
         return settings.REPOSITORY_PATH / self.slug
+
+    @property
+    def bundle(self) -> Iterator[str]:
+        f = open(self.path / '.bundle')
+        for line in f:
+            globs = glob.glob(line.strip(), root_dir=self.path)
+            for g in globs:
+                yield g
+
+    @property
+    def zipname(self) -> str:
+        return f'{self.slug}.zip'
+
+    def build_full_path(self, relative_path: str) -> Path:
+        return self.path / relative_path
+
+    def zip(self) -> io.BytesIO:
+        buffer = io.BytesIO()
+        with zipfile.ZipFile(buffer, 'w') as archive:
+            for f in self.bundle:
+                archive.write(self.build_full_path(f), f'{self.slug}/{f}')
+        buffer.seek(0)
+        return buffer
 
     def __str__(self):
         return self.slug
--- exercises/views.py
+++ exercises/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+
+from .models import Exercise
 
 
 def get_exercise(request, slug: str):
-    return HttpResponse('Good luck!')
+    exercise = get_object_or_404(Exercise, slug=slug)
+    response = HttpResponse(exercise.zip(), content_type='application/zip')
+    response['Content-Disposition'] = f'attachment; filename={exercise.zipname}'
+
+    return response
