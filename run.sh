1d8f91294ca90949f77e3e0c13a07d7c905e90a0
Sergio Delgado Quintero <sdelquin@gmail.com>
2024-05-24 12:40:16 +0100

    Fix typo

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 cd $(dirname $0)
 source ~/.pyenv/versions/pypas-web/bin/activate
-exec gunicorn -b unix:/tmp/pypas-web.sock main.wsgi:application
+exec gunicorn -b unix:/tmp/pypas-web.sock pypas.wsgi:application
