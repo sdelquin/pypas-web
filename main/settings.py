818cc274d3f8d9c86942f731748e9db85f8e47af
Sergio Delgado Quintero <sdelquin@gmail.com>
2024-05-28 10:50:50 +0100

    Change exercise config file naming

--- main/settings.py
+++ main/settings.py
@@ -132,4 +132,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REPOSITORY_PATH = config('REPOSITORY_PATH', default=BASE_DIR / 'repository', cast=Path)
 
-EXERCISE_CONFIG_FILE = config('EXERCISE_CONFIG_FILE', default='.config.toml')
+EXERCISE_CONFIG_FILE = config('EXERCISE_CONFIG_FILE', default='.pypas.toml')
