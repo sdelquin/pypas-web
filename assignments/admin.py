90750f1c3bc027d343068b2b7f64c2475e0f880c
Sergio Delgado Quintero <sdelquin@gmail.com>
2024-06-13 19:24:54 +0100

    Add frames

--- assignments/admin.py
+++ assignments/admin.py
@@ -1,12 +1,23 @@
 from django.contrib import admin
 
-from .models import Assignment
+from .models import Assignment, Bucket, Frame
 
 
 @admin.register(Assignment)
 class AssignmentAdmin(admin.ModelAdmin):
-    list_display = ['user_alias', 'exercise', 'passed', 'created_at', 'updated_at']
+    list_display = ['user_alias', 'exercise', 'passed', 'frame', 'created_at', 'updated_at']
 
     @admin.display(description='User')
     def user_alias(self, obj):
         return obj.user.alias
+
+
+@admin.register(Frame)
+class FrameAdmin(admin.ModelAdmin):
+    list_display = ['context', 'bucket', 'start', 'end', 'num_exercises']
+
+
+@admin.register(Bucket)
+class BucketAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
--- assignments/models.py
+++ assignments/models.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+
+import datetime
 import shlex
 import shutil
 import subprocess
@@ -8,6 +11,8 @@ import pytest
 from django.conf import settings
 from django.db import models
 
+from access.models import Context
+
 
 class Assignment(models.Model):
     user = models.ForeignKey('access.User', on_delete=models.PROTECT, related_name='assignments')
@@ -17,6 +22,13 @@ class Assignment(models.Model):
     passed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    frame = models.ForeignKey(
+        'assignments.Frame',
+        on_delete=models.PROTECT,
+        related_name='assignments',
+        null=True,
+        blank=True,
+    )
 
     class Meta:
         unique_together = ('user', 'exercise')
@@ -37,3 +49,37 @@ class Assignment(models.Model):
 
     def __str__(self):
         return f'{self.user} at {self.exercise}'
+
+    def save(self, *args, **kwargs):
+        if self.frame is None:
+            self.frame = Frame.get_active_frame(self.user.context)
+        super().save(*args, **kwargs)
+
+
+class Frame(models.Model):
+    context = models.ForeignKey('access.Context', on_delete=models.CASCADE, related_name='frames')
+    bucket = models.ForeignKey(
+        'assignments.Bucket', on_delete=models.CASCADE, related_name='frames'
+    )
+    start = models.DateField()
+    end = models.DateField()
+    num_exercises = models.PositiveSmallIntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.context} ({self.bucket})'
+
+    @classmethod
+    def get_active_frame(cls, context: Context) -> Frame | None:
+        today = datetime.date.today()
+        try:
+            return cls.objects.get(start__lte=today, end__gte=today, context=context)
+        except Frame.DoesNotExist:
+            return None
+
+
+class Bucket(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=256, unique=True)
+
+    def __str__(self):
+        return self.name
