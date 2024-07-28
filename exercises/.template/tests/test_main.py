c9fa7c6b03af7f5006850effbb6f73f47f34273a
Sergio Delgado Quintero <sdelquin@gmail.com>
2024-07-28 19:50:07 +0100

    Add testdata skeleton to test template

--- exercises/.template/tests/test_main.py
+++ exercises/.template/tests/test_main.py
@@ -1,10 +1,19 @@
 import os
 
+import pytest
+
 if os.path.exists('solution.py'):
     import solution as main
 else:
     import main  # type:ignore
 
+testdata = [
+    (1, 1),
+    (2, 4),
+    (3, 9),
+]
 
-def test_run():
-    assert main.run() is True
+
+@pytest.mark.parametrize('x, expected', testdata)
+def test_run(x, expected):
+    assert main.run(x) == expected
