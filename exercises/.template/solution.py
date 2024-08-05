0d3199a1786afd07fa9b6a8d736a1fc2b3898f3f
Sergio Delgado Quintero <sdelquin@gmail.com>
2024-08-05 17:54:53 +0100

    Add code to solution template to run it as main

--- exercises/.template/solution.py
+++ exercises/.template/solution.py
@@ -1,2 +1,16 @@
 def run():
     pass  # TODO
+
+
+# DO NOT TOUCH THE CODE BELOW
+if __name__ == '__main__':
+    import inspect
+
+    try:
+        import args
+    except ModuleNotFoundError:
+        print('You have to create "args.py" with the main function arguments!')
+    else:
+        params = inspect.signature(run).parameters.keys()
+        kwargs = {p: args.__dict__.get(p) for p in params}
+        run(**kwargs)  # type: ignore
