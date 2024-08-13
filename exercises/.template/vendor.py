1f3da441dbfa307e453cbeb061c33ea9d173400d
Sergio Delgado Quintero <sdelquin@gmail.com>
2024-08-13 10:30:48 +0100

    Fix running when no args are needed

--- exercises/.template/vendor.py
+++ exercises/.template/vendor.py
@@ -11,18 +11,21 @@ import rich
 
 def launch(func: typing.Callable):
     """Launch func looking its args within args.py file"""
-    try:
-        import args
-    except ModuleNotFoundError:
-        rich.print('[red]You have to create [b]args.py[/b] with the main function arguments!')
-    else:
-        user_args = args.__dict__
-        kwargs = {}
-        for param in inspect.signature(func).parameters.keys():
-            if param not in user_args:
-                rich.print(f'[red]Parameter [b]{param}[/b] must be defined in [b]args.py[/b]')
-                rich.print(f'[dim italic]{param} = value')
-                break
-            kwargs[param] = user_args[param]
+    if func_params := inspect.signature(func).parameters.keys():
+        try:
+            import args
+        except ModuleNotFoundError:
+            rich.print('[red]You have to create [b]args.py[/b] with the main function arguments!')
         else:
-            func(**kwargs)  # type: ignore
+            user_args = args.__dict__
+            kwargs = {}
+            for param in func_params:
+                if param not in user_args:
+                    rich.print(f'[red]Parameter [b]{param}[/b] must be defined in [b]args.py[/b]')
+                    rich.print(f'[dim italic]{param} = value')
+                    break
+                kwargs[param] = user_args[param]
+            else:
+                func(**kwargs)  # type: ignore
+    else:
+        func()
