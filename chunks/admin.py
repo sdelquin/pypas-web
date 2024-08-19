fde0b54d09696197fe86797cfd1d773a8b63d880
Sergio Delgado Quintero <sdelquin@gmail.com>
2024-08-19 19:37:45 +0100

    Add filters to admin panel for chunks

--- chunks/admin.py
+++ chunks/admin.py
@@ -5,12 +5,16 @@ from .models import Chunk
 
 @admin.register(Chunk)
 class ChunkAdmin(admin.ModelAdmin):
-    list_display = ['frame', 'exercise', 'puttable', 'int_order']
+    list_display = ['frame', 'exercise', 'exercise_topic', 'puttable', 'int_order']
     autocomplete_fields = ['exercise']
     search_fields = ['exercise__slug']
-    list_filter = ['frame__context', 'frame__bucket', 'puttable']
+    list_filter = ['frame__context', 'frame__bucket', 'exercise__topic', 'puttable']
 
     @admin.display(description='Order')
     def int_order(self, obj) -> int:
         iorder = int(obj.order)
         return iorder if iorder == obj.order else obj.order
+
+    @admin.display(description='Topic')
+    def exercise_topic(self, obj) -> str:
+        return obj.exercise.topic
